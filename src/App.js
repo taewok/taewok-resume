f0ab1c4bcb75e8cd45ccf0ff02b9ff466db95b6a
taewok <taewok5163@naver.com>
2023-10-11 01:21:32 +0900

    feat: 보기 편한 구성을 위치 변경과 Skills 내용 추가

--- src/App.js
+++ src/App.js
@@ -9,67 +9,73 @@ function App() {
           <h1>
             안녕하세요~!
             <br />
-            프론트엔드 개발자 김태욱입니다.
+            프론트엔드 개발자 "김태욱"입니다.
           </h1>
         </section>
+        <div className="myprofile-box">
+          <ul className="myinfo-list">
+            <h2>📬 Contact. & Channel.</h2>
+            <li className="myinfo-item">
+              <span>E-mail</span>:
+              <a href="mailto:taewok51615@gmail.com">taewok51615@gmail.com</a>
+            </li>
+            <li className="myinfo-item">
+              <span>Phone</span>:<a href="tel:010-2911-4961">010-2911-4961</a>
+            </li>
+            <li className="myinfo-item">
+              <span>Github</span>:
+              <a
+                href="https://github.com/taewok"
+                target="_blank"
+                rel="noreferrer"
+              >
+                https://github.com/taewok
+              </a>
+            </li>
+            <li className="myinfo-item">
+              <span>Blog</span>:
+              <a
+                href="https://taewok.github.io/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                https://taewok.github.io
+              </a>
+            </li>
+            <li className="myinfo-item">
+              <span>Portfolio</span>:
+              <a
+                href="https://taewok-portfolio.netlify.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                https://taewok-portfolio.netlify.app
+              </a>
+            </li>
+          </ul>
+          {/* <div className="myimg-box">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQH62fzQwFuoNZKHymSnGCnocX2NzSAiX9t5w&usqp=CAU" />
+            </div> */}
+        </div>
         <section className="introduce-box">
           <h2>🙇 About Me.</h2>
           <div className="introduce-text-box">
-            안녕하세요 프론트엔드 개발자를 꿈꾸는 "김태욱"입니다.
-            <br />
-            <br />
-            저는 코드의 성능 향상을 위한 코드와 알고리즘에 관심이 많아
-            적극적으로
-            <br />
-            학습하고 인상 깊은 코드 혹은 기술은 블로그를 통해 기록하며 자신의
-            것으로
-            <br />
-            만들어가며 성장하고 있습니다.
-            <br />
-            <br />
-            사용자에게 편하고 부드러운 인터페이스와 경험을 중요하게 생각하며
-            그러기
-            <br />
-            위한 과정에서 팀원간의 소통을 중요시하며 재사용이 용이한 코드
-            후일에도
-            <br />
-            이해하기 쉬운 코드를 쓰려고 노력합니다.
-          </div>
-          <div className="myprofile-box">
-            <ul className="myinfo-list">
-              <h3>Contact.</h3>
-              <li className="myinfo-item">
-                <span>📧E-mail</span>:
-                <a href="mailto:taewok51615@gmail.com">taewok51615@gmail.com</a>
-              </li>
-              <li className="myinfo-item">
-                <span>📞Phone</span>:
-                <a href="tel:010-2911-4961">010-2911-4961</a>
-              </li>
-              <li className="myinfo-item">
-                <span>👨‍🔧Github</span>:
-                <a
-                  href="https://github.com/taewok"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  https://github.com/taewok
-                </a>
-              </li>
-              <li className="myinfo-item">
-                <span>📑Blog</span>:
-                <a
-                  href="https://taewok.github.io/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  https://taewok.github.io
-                </a>
-              </li>
-            </ul>
-            {/* <div className="myimg-box">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQH62fzQwFuoNZKHymSnGCnocX2NzSAiX9t5w&usqp=CAU" />
-            </div> */}
+            <p>
+              안녕하세요 2년간 개발에 열정을 갖고 꾸준한 노력으로 매일 성장해온
+              <br /> 프론트엔드 개발자 <b>"김태욱"</b>입니다.
+              <br />
+              <br />
+              저는 코드의 성능 향상을 위한 코드와 알고리즘에 관심이 많아
+              적극적으로 학습하며 인상 깊은 코드 혹은 기술은 Github 블로그를
+              통해 기록해가며 성장하고 있습니다.
+              <br />
+              <br />
+              사용자에게 편하고 부드러운 인터페이스와 경험을 중요하게 생각하며
+              그러기 위한 과정에서 팀원 간의 소통을 핵심으로 여기고 있으며
+              <br />
+              항상 재사용이 용이한 코드와 이해하기 쉬운 코드를 쓰려고 노력하고
+              있습니다.
+            </p>
           </div>
         </section>
         <section className="projects-box">
@@ -95,7 +101,7 @@ function App() {
                   https://www.gridians.site
                 </a>
               </p>
-              <b>GithubSite</b>
+              <b>Github</b>
               <p className="project-github-url">
                 <a href="https://github.com/gridians/gridians-fe">
                   https://github.com/gridians/gridians-fe
@@ -113,11 +119,11 @@ function App() {
               <h4 className="center-text">[ 구현 내용 ]</h4>
               <ul className="role-list">
                 <li className="role-item">
-                  <b>react-query</b>와 <b>throttle</b>를 사용해 일정 스크롤시
-                  데이터를 추가로 받아오는 무한스크롤
+                  react-query와 throttle를 사용해 일정 스크롤시 데이터를 추가로
+                  받아오는 무한스크롤
                 </li>
                 <li className="role-item">
-                  <b>Github Rest API</b>를 활용한 <b>github</b>연동 및 정보 표현
+                  Github Rest API를 활용한 github연동 및 정보 표현
                 </li>
                 <li className="role-item">
                   react-github-calendar 라이브러리를 사용해 잔디 생성
@@ -189,7 +195,7 @@ function App() {
                   https://twogather.netlify.app
                 </a>
               </p>
-              <b>GithubSite</b>
+              <b>Github</b>
               <p className="project-github-url">
                 <a href="https://github.com/TWOGATH3R/twogather-web-frontend">
                   https://github.com/TWOGATH3R/twogather-web-frontend
@@ -210,28 +216,28 @@ function App() {
               <h4 className="center-text">[ 구현 내용 ]</h4>
               <ul>
                 <li className="role-item">
-                  axios interceptors, create을 활용한 반복 코드 간결화
-                </li>
-                <li className="role-item">
                   쿼리스트링을 활용한 url을 변경하는 것 만으로 원하는 검색결과와
                   page 결과 얻기
-                </li>
-                <li className="role-item">
-                  지역, 카테고리, 메뉴 더보기, 사유 입력 등 모달창 컴포넌트 구현
-                </li>
-                <li className="role-item">
-                  로그인, 회원가입, pw찾기, id찾기, 댓글/대댓글 등록/삭제/수정
-                  구현
                 </li>
                 <li className="role-item">
                   로그인, 회원가입, pw/id찾기, admin, home, mypage, 검색결과
                   페이지 퍼블리싱
                 </li>
                 <li className="role-item">
-                  로컬 스토리지에 저장된 권한에 따른 ui와 기능
+                  로그인, 회원가입, pw찾기, id찾기, 댓글/대댓글 등록/삭제/수정
+                  구현
+                </li>
+                <li className="role-item">
+                  지역, 카테고리, 메뉴 더보기, 사유 입력 등 모달창 컴포넌트 구현
                 </li>
                 <li className="role-item">
                   pagenation, Filter 컴포넌트를 따로 분리하여 재사용성 상승
+                </li>
+                <li className="role-item">
+                  axios interceptors, create을 활용한 반복 코드 간결화
+                </li>
+                <li className="role-item">
+                  로컬 스토리지에 저장된 권한에 따른 ui와 기능
                 </li>
                 <li className="role-item">
                   react-cookie를 활용한 accessToken 관리
@@ -306,48 +312,52 @@ function App() {
           <h3 className="skill-name">HTML / CSS</h3>
           <ul className="skill-explain-list">
             <li className="skill-explain-item">
+              프로젝트의 디자인에 알맞은 스타일 적용
+            </li>
+            <li className="skill-explain-item">
+              CSS의 가독성을 높이고, 유지보수에 용이한 SCSS 사용
+            </li>
+            <li className="skill-explain-item">
               시맨틱 마크업에 대한 이해를 바탕으로 웹표준을 준수한 레이아웃 설계
             </li>
             <li className="skill-explain-item">
               유동 그리드 레이아웃, 이미지, 미디어 쿼리 등을 이용한 반응형 웹
               구현
             </li>
-            <li className="skill-explain-item">
-              CSS의 가독성을 높이고, 유지보수에 용이한 SCSS 사용
-            </li>
-            <li className="skill-explain-item">
-              프로젝트의 디자인에 알맞은 스타일 적용
-            </li>
           </ul>
           <h3 className="skill-name">TypeScript / JavaScript</h3>
           <ul className="skill-explain-list">
+            <li className="skill-explain-item">ES6 이상 문법을 적극 사용</li>
             <li className="skill-explain-item">
-              JS, TS를 이용한 연산과 DOM 객체의 조작
+              Vanilla JS를 이용한 DOM 객체의 조작
             </li>
             <li className="skill-explain-item">
-              async/await 등을 활용한 비동기 작업 처리
+              Axios 기반의 Rest API 서버 통신 및 데이터 처리
             </li>
             <li className="skill-explain-item">
-              Rest API 기반의 서버 통신 및 데이터 처리
+              async/await, Promise 등을 활용한 비동기 작업 처리
+            </li>
+            <li className="skill-explain-item">
+              인터페이스와 타입 정의를 사용하여 코드의 가독성과 재사용성 상승
             </li>
             <li className="skill-explain-item">
               타입스크립트의 정적 타입 검사를 활용하여 코드 안정성을 높이고
               런타임 에러를 사전에 방지
             </li>
-            <li className="skill-explain-item">
-              인터페이스와 타입 정의를 사용하여 코드의 가독성과 재사용성 상승
-            </li>
-            <li className="skill-explain-item">함수형 프로그래밍 지향</li>
           </ul>
           <h3 className="skill-name">React</h3>
           <ul className="skill-explain-list">
+            <li className="skill-explain-item">JSX 문법 이해</li>
+            <li className="skill-explain-item">다양한 라이브러리 응용</li>
+            <li className="skill-explain-item">
+              컴포넌트의 재사용성을 고려한 컴포넌트 구현
+            </li>
             <li className="skill-explain-item">
               React Hook을 통한 변수 관리와 라이프 사이클에 적절한 활용
             </li>
             <li className="skill-explain-item">
-              컴포넌트의 재사용성을 고려한 컴포넌트 구현
+              리액트 라우터(React Router)를 이용한 모듈단위 라우팅 구조 설계
             </li>
-            <li className="skill-explain-item">다양한 라이브러리 응용</li>
           </ul>
         </section>
         <section className="experience-box">
